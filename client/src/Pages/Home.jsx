a7538ca56f75e2d0a162a531147cc88e7b6c695d
shagorpixel <shagorpixel@gmail.com>
2025-07-19 13:17:44 +0600

    Add Best Seller Section

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Components/Hero';
 import LeatestCollection from '../Components/LeatestCollection';
+import BestSeller from '../Components/BestSeller';
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <LeatestCollection />
+            <BestSeller />
         </div>
     );
 };
