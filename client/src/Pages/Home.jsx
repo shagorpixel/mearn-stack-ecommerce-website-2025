be6cdc428f8529fa01ff77fef98726a4d36c0153
shagorpixel <shagorpixel@gmail.com>
2025-07-19 13:19:13 +0600

    Add Our Policy Component

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../Components/Hero';
 import LeatestCollection from '../Components/LeatestCollection';
 import BestSeller from '../Components/BestSeller';
+import OurPolicy from '../Components/OurPolicy';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Hero />
             <LeatestCollection />
             <BestSeller />
+            <OurPolicy />
         </div>
     );
 };
